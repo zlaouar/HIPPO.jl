ddc07c61f9e866318eb3571244ad094d453a94b4
Zakariya Laouar <zala9003@colorado.edu>
2022-10-19 13:20:12 -0600

    added terminal state

--- src/HIPPO.jl
+++ src/HIPPO.jl
@@ -8,6 +8,7 @@ using Random
 using StaticArrays
 using Parameters
 using LinearAlgebra
+using Compose
 
 export 
     TargetSearchPOMDP,
@@ -18,20 +19,30 @@ struct TSState
     target::SVector{2, Int}
 end
 
+struct roi
+    maploc::SVector{2, Int}
+    prob::Float64
+end
+
 struct TargetSearchPOMDP <: POMDP{TSState, Symbol, BitArray{1}}
     size::SVector{2, Int}
     obstacles::Set{SVector{}}
     robot_init::SVector{2, Int}
     tprob::Float64
     targetloc::SVector{2, Int}
+    #rois::Set{}
     #obsindices::Array{Union{Nothing,Int}, 4}
 end
 
-function TargetSearchPOMDP(;size=(10,10), n_obstacles=8, rng::AbstractRNG=Random.MersenneTwister(20))
+function TargetSearchPOMDP(;roipoints=[], size=(10,10), n_obstacles=8, rng::AbstractRNG=Random.MersenneTwister(20))
     obstacles = Set{SVector{2, Int}}()
     robot_init = SVector(rand(rng, 1:size[1]), rand(rng, 1:size[2]))
     tprob = 0.7
     targetloc = SVector(size)
+    #= for state in roisstates
+        push!(rois, TSState(robot_init, state))
+    end
+    rois = TSState[] =#
 
     TargetSearchPOMDP(size, obstacles, robot_init, tprob, targetloc)
 end
@@ -54,31 +65,40 @@ function bounce(m::TargetSearchPOMDP, pos, offset)
 end
 
 function POMDPs.transition(m::TargetSearchPOMDP, s, a)
-    robotlocs = [TSState(s.robot, m.targetloc)]
-    robotprobs = Float64[0.0]
+    if s.robot == s.target
+        return Deterministic(TSState([-1,-1], [-1,-1]))
+    end
+
+    states = [TSState(s.robot, m.targetloc)]
+    probs = Float64[0.0]
     
+    #= if s.robot in m.rois
+        push!(states, TSState(SA[-1,-1], SA[-1,-1])) # terminal state for regions of interest
+        push!(probs, s.rois[s.robot].prob)
+    end =#
+
     for change in actionvals
         newrobot = bounce(m, s.robot, change)
 
         if change == actiondir[a]
             if newrobot == s.robot # robot bounced off wall 
-                robotprobs[1] += m.tprob
+                probs[1] += m.tprob
             else 
-                push!(robotprobs, m.tprob)
-                push!(robotlocs, TSState(newrobot, m.targetloc))
+                push!(probs, m.tprob)
+                push!(states, TSState(newrobot, m.targetloc))
             end
         else
             tprob = (1-m.tprob)/(length(actions(m))-1)
             if newrobot == s.robot # robot bounced off wall 
-                robotprobs[1] += tprob
+                probs[1] += tprob
             else 
-                push!(robotprobs, tprob)
-                push!(robotlocs, TSState(newrobot, m.targetloc))
+                push!(probs, tprob)
+                push!(states, TSState(newrobot, m.targetloc))
             end
         end
     end
     
-    return SparseCat(robotlocs, robotprobs)
+    return SparseCat(states, probs)
 
 end
 
@@ -94,14 +114,38 @@ function POMDPs.initialstate(m::TargetSearchPOMDP)
     return POMDPTools.Uniform(TSState(m.robot_init, SVector(x, y)) for x in 1:m.size[1], y in 1:m.size[2])
 end
 
-#= function POMDPTools.ModelTools.render(m::TargetSearchPOMDP, step)
+function POMDPTools.ModelTools.render(m::TargetSearchPOMDP, step)
     nx, ny = m.size
     cells = []
     target_marginal = zeros(nx, ny)
-    if haskey(step, :bp)
+    
+    for x in 1:nx, y in 1:ny
+        cell = cell_ctx((x,y), m.size)
+        target = compose(context(), rectangle(), stroke("gray"))
+        compose!(cell, target)
+        push!(cells, cell)
+    end
+    grid = compose(context(), linewidth(0.5mm), cells...)
+    outline = compose(context(), linewidth(1mm), rectangle(), fill("white"), stroke("gray"))
 
-end =#
+    if haskey(step, :sp)
+        robot_ctx = cell_ctx(step[:sp].robot, m.size)
+        robot = compose(robot_ctx, circle(0.5, 0.5, 0.5), fill("green"))
+        target_ctx = cell_ctx(step[:sp].target, m.size)
+        target = compose(target_ctx, circle(0.5, 0.5, 0.5), fill("orange"))
+    else
+        robot = nothing
+        target = nothing
+    end
+    sz = min(w,h)
+    return compose(context((w-sz)/2, (h-sz)/2, sz, sz), robot, target, grid, outline)
+end
 
+function cell_ctx(xy, size)
+    nx, ny = size
+    x, y = xy
+    return context((x-1)/nx, (ny-y)/ny, 1/nx, 1/ny)
+end
 POMDPs.isterminal(m::TargetSearchPOMDP, s::TSState) = s.target == s.robot
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using HIPPO
 using POMDPs
+using QMDP
 using BasicPOMCP
 using POMDPTools
 
@@ -7,7 +8,11 @@ m = TargetSearchPOMDP()
 
 transition(m, TSState([1,1], [10,10]), :left)
 
-solver = POMCPSolver()
+#solver = POMCPSolver()
+solver = QMDPSolver(max_iterations=20,
+                    belres=1e-3,
+                    verbose=true
+                   ) 
 planner = solve(solver, m)
 
 ds = DisplaySimulator()
