ec2d921a77578fadd66b35496b869bc9f763c76d
Zakariya Laouar <zala9003@colorado.edu>
2022-12-19 17:04:32 -0700

    hunter data

---
+++ test/test_ncar.jl
@@ -0,0 +1,9 @@
+using Serialization
+using DataFrames
+struct map_database
+    ID2grid :: Dict{Int64, Tuple{Int64, Int64}}
+    grid2ID::Dict{Tuple{Int64, Int64}, Int64}
+    reward::Array{Float64}
+    features::DataFrame
+end
+data = deserialize(joinpath(@__DIR__, "operator_database_NCAR_watertrail.dat"))
