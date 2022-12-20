fed1db1a923a862c203f82babe97befd6b8b9b81
Zakariya Laouar <zala9003@colorado.edu>
2022-12-19 17:02:24 -0700

    cleanup

--- test/debug.jl
+++ test/debug.jl
@@ -10,7 +10,7 @@ using ParticleFilters
 
 sleep_until(t) = sleep(max(t-time(), 0.0))
 
-function custom_sim(m::TargetSearchPOMDP, m1::TargetSearchPOMDP, planner, up, b, sinit)
+function custom_sim(msolve::TargetSearchPOMDP, msim::TargetSearchPOMDP, planner, up, b, sinit)
     r_total = 0.0
     s = sinit
     o = Nothing
@@ -19,14 +19,14 @@ function custom_sim(m::TargetSearchPOMDP, m1::TargetSearchPOMDP, planner, up, b,
     dt = 1/max_fps
     d = 1.0
     sim_states = TSState[]
-    while !isterminal(m1, s)
+    while !isterminal(msim, s)
         tm = time()
         a = action(planner, b)
-        s, o, r = @gen(:sp,:o,:r)(m1, s, a)
+        s, o, r = @gen(:sp,:o,:r)(msim, s, a)
         r_total += d*r
-        d *= discount(m1)
+        d *= discount(msim)
         b = update(up, b, a, o)
-        display(render(m1, (sp=s, bp=b)))
+        display(render(msim, (sp=s, bp=b)))
         sleep_until(tm += dt)
         iter += 1
         println(iter)
@@ -34,8 +34,8 @@ function custom_sim(m::TargetSearchPOMDP, m1::TargetSearchPOMDP, planner, up, b,
             roi_states = [[1,9],[1,10],[1,8]]
             probs = [0.8,0.8,0.8]
             roi_points = Dict(roi_states .=> probs)
-            m.rois = roi_points
-            planner = solve(solver, m)
+            msolve.rois = roi_points
+            planner = solve(solver,msolve)
         end
         push!(sim_states, s)
     end
@@ -43,22 +43,19 @@ function custom_sim(m::TargetSearchPOMDP, m1::TargetSearchPOMDP, planner, up, b,
 end
 
 
-sinit = TSState([10,1],[2,7])#rand(initialstate(m1))
+sinit = TSState([10,1],[2,7])#rand(initialstate(msim))
 roi_states = [[7,9],[7,10],[7,8]]
 probs = [0.8,0.8,0.8]
 roi_points = Dict(roi_states .=> probs)
-#m = TargetSearchPOMDP(roi_points=roi_points)
-m = TargetSearchPOMDP(sinit=sinit)
+#msolve= TargetSearchPOMDP(roi_points=roi_points)
+msolve= TargetSearchPOMDP(sinit=sinit)
 mdp_solver = ValueIterationSolver() # creates the solver
-mdp_policy = solve(mdp_solver, UnderlyingMDP(m))
+mdp_policy = solve(mdp_solver, UnderlyingMDP(msolve))
 
 
 
 #estimate_value=FORollout(mdp_policy)
-#= function mypol(m::TargetSearchPOMDP, s, h::BeliefNode, steps)
-    if s.
 
-end =#
 solver = POMCPSolver(estimate_value=FORollout(mdp_policy), tree_queries=10000, max_time=0.2, c=3)
 #solver = POMCPSolver(tree_queries=10000, max_time=0.2, c=3)
 #solver = QMDPSolver(max_iterations=20,
@@ -66,29 +63,29 @@ solver = POMCPSolver(estimate_value=FORollout(mdp_policy), tree_queries=10000, m
 #                    verbose=true
 #                   ) 
 
-planner = solve(solver, m)
+planner = solve(solver,msolve)
 
 ds = DisplaySimulator()
 hr = HistoryRecorder()
-m1 = TargetSearchPOMDP(sinit=sinit)
+msim = TargetSearchPOMDP(sinit=sinit)
 
-b0 = initialstate(m)
-up = DiscreteUpdater(m)
+b0 = initialstate(msolve)
+up = DiscreteUpdater(msolve)
 b = initialize_belief(up, b0)
 
 N = 1000
-particle_up = BootstrapFilter(m, N)
+particle_up = BootstrapFilter(msolve, N)
 particle_b = initialize_belief(particle_up, b0)
 
 
 
 
 
-s,r_total,sim_states  = custom_sim(m, m1, planner, particle_up, particle_b, sinit)
+s,r_total,sim_states  = custom_sim(msolve, msim, planner, particle_up, particle_b, sinit)
 
 r_total
-#h = simulate(ds, m1, planner)
-#h = simulate(ds, m1, planner, particle_up, initialstate(m1), sinit)
+#h = simulate(ds, msim, planner)
+#h = simulate(ds, msim, planner, particle_up, initialstate(msim), sinit)
 
 
 #@profview action(planner, initialstate(m))
