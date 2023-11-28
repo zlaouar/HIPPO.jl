962b3c13c9bb46149983f105b7373a3187903591
Zakariya Laouar <zala9003@colorado.edu>
2023-11-27 18:13:27 -0700

    reorg

---
+++ src/pachPOMDP/observations.jl
@@ -0,0 +1,44 @@
+"""
+    observations(m::TargetSearchPOMDP)
+
+Retrieve observations in TargetSearch observation space
+
+The the observations are ordered as follows:
+    1: The target is not observed
+    2: The target is in same grid cell as robot
+    3: The target is to the left of the robot
+    4: The target is to the right of the robot
+    5: The target is below the robot
+    6: The target is above the robot
+"""
+POMDPs.observations(m::TargetSearchPOMDP) = OBSERVATIONS #vec(collect(BitVector([c[1],c[2],c[3],c[4],c[5]]) for c in Iterators.product(0:1, 0:1, 0:1, 0:1, 0:1)))
+POMDPs.obsindex(m::TargetSearchPOMDP, o::BitVector) = obsind[o]
+
+function POMDPs.observation(m::TargetSearchPOMDP, a::Symbol, sp::TSState)
+    #obs = [BitVector([0,0,0,0,0]), BitVector([1,0,0,0,0]), BitVector([0,1,0,0,0]), BitVector([0,0,1,0,0]), BitVector([0,0,0,1,0]), BitVector([0,0,0,0,1])]
+
+    if norm(sp.robot-sp.target) == 1.0 # target and robot within one grid cell of each other 
+        targetloc = targetdir(sp)
+
+        if targetloc == :left
+            probs = [0.0, 0.0, 0.80, 0.0, 0.10, 0.10]
+        elseif targetloc == :right
+            probs = [0.0, 0.0, 0.0, 0.80, 0.10, 0.10]
+        elseif targetloc == :up
+            probs = [0.0, 0.0, 0.10, 0.10, 0.0, 0.80]
+        elseif targetloc == :down
+            probs = [0.0, 0.0, 0.10, 0.10, 0.80, 0.0]
+        end
+
+        return SparseCat(OBSERVATIONS, probs)
+    end
+
+    if sp.robot == sp.target # target and robot in same grid cell
+        probs = [0.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+        return SparseCat(OBSERVATIONS, probs)
+    end
+
+    probs = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    return SparseCat(OBSERVATIONS, probs)
+
+end
